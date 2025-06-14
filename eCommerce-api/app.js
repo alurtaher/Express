435b58aaec0fe53873f532c3f3c8615fb9312e7d
A Taher Basha <taherbashaengineer@gmail.com>
2025-06-15 01:09:53 +0530

    Get Request with HTML

--- eCommerce-api/app.js
+++ eCommerce-api/app.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userRoutes = require('./routes/userRoutes.js');
-const productRoutes = require('./routes/productRoutes.js');
-const cartRoutes = require('./routes/cartRoutes.js');
+const userRoutes = require("./routes/userRoutes.js");
+const productRoutes = require("./routes/productRoutes.js");
+const cartRoutes = require("./routes/cartRoutes.js");
+const path = require("path");
 
 app.use(express.json());
 
-app.use('/users', userRoutes);
-app.use('/products', productRoutes);
-app.use('/cart', cartRoutes);
+//Sending the HTML File
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "product.html"));
+});
 
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
 
-
-const PORT =3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
